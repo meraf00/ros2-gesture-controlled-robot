235e3114bd9b8c45a0022240f50fff19408c2098
meraf00 <meraf000@gmail.com>
2024-02-06 05:54:54 +0300

    feat: support for wsl

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import csv
 import copy
 import itertools
 
+import socket, cv2, pickle, struct
+
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 from model import KeyPointClassifier
 
 
-def main():
+def main():    
     use_brect = True
 
     # Camera preparation ###############################################################
-    cap = cv.VideoCapture(0)
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+    
 
     # Model load #############################################################
     mp_hands = mp.solutions.hands
@@ -40,62 +44,144 @@ def main():
     #  ########################################################################
     mode = 0
 
-    while True:
-        # Process Key (ESC: end) #################################################
-        key = cv.waitKey(10)
-        if key == 27:  # ESC
-            break
-        number, mode = select_mode(key, mode)
+    if os.getenv('environment') == 'wsl':
+        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        host_ip = '192.168.56.1'
+        port = 9999
+        client_socket.connect((host_ip,port))
+        data = b""
+        payload_size = struct.calcsize("Q")
 
-        # Camera capture #####################################################
-        ret, image = cap.read()
-        if not ret:
-            break
-        image = cv.flip(image, 1)  # Mirror display
-        debug_image = copy.deepcopy(image)
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4*1024) # 4K
+                if not packet: break
+                data+=packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q",packed_msg_size)[0]
+            
+            while len(data) < msg_size:
+                data += client_socket.recv(4*1024)
+            frame_data = data[:msg_size]
+            data  = data[msg_size:]
+            image = pickle.loads(frame_data)
+            
 
-        # Detection implementation #############################################################
-        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+            # Process Key (ESC: end) #################################################
+            key = cv.waitKey(10)
+            if key == 27:  # ESC
+                break
+            number, mode = select_mode(key, mode)
 
-        image.flags.writeable = False
-        results = hands.process(image)
-        image.flags.writeable = True
+            # Camera capture #####################################################                        
+            image = cv.flip(image, 1)  # Mirror display
+            debug_image = copy.deepcopy(image)
 
-        #  ####################################################################
-        if results.multi_hand_landmarks is not None:
-            for hand_landmarks, handedness in zip(
-                results.multi_hand_landmarks, results.multi_handedness
-            ):
-                # Bounding box calculation
-                brect = calc_bounding_rect(debug_image, hand_landmarks)
-                # Landmark calculation
-                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+            # Detection implementation #############################################################
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-                # Conversion to relative coordinates / normalized coordinates
-                pre_processed_landmark_list = pre_process_landmark(landmark_list)
-                # Write to the dataset file
-                logging_csv(number, mode, pre_processed_landmark_list)
+            image.flags.writeable = False
+            results = hands.process(image)
+            image.flags.writeable = True
 
-                # Hand sign classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+            #  ####################################################################
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks, handedness in zip(
+                    results.multi_hand_landmarks, results.multi_handedness
+                ):
+                    # Bounding box calculation
+                    brect = calc_bounding_rect(debug_image, hand_landmarks)
+                    # Landmark calculation
+                    landmark_list = calc_landmark_list(debug_image, hand_landmarks)
 
-                # Drawing part
-                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
-                debug_image = draw_landmarks(debug_image, landmark_list)
-                debug_image = draw_info_text(
-                    debug_image,
-                    brect,
-                    handedness,
-                    keypoint_classifier_labels[hand_sign_id],
-                )
+                    # Conversion to relative coordinates / normalized coordinates
+                    pre_processed_landmark_list = pre_process_landmark(landmark_list)
+                    # Write to the dataset file
+                    logging_csv(number, mode, pre_processed_landmark_list)
 
-        debug_image = draw_info(debug_image, mode, number)
+                    # Hand sign classification
+                    hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
 
-        # Screen reflection #############################################################
-        cv.imshow("Hand Gesture Recognition", debug_image)
+                    # Drawing part
+                    debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                    debug_image = draw_landmarks(debug_image, landmark_list)
+                    debug_image = draw_info_text(
+                        debug_image,
+                        brect,
+                        handedness,
+                        keypoint_classifier_labels[hand_sign_id],
+                    )
 
-    cap.release()
-    cv.destroyAllWindows()
+            debug_image = draw_info(debug_image, mode, number)
+
+            # Screen reflection #############################################################
+            cv.imshow("Hand Gesture Recognition", debug_image)
+
+        cap.release()
+        cv.destroyAllWindows()
+
+    else:
+        cap = cv.VideoCapture(0)
+        cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+
+        while True:
+            # Process Key (ESC: end) #################################################
+            key = cv.waitKey(10)
+            if key == 27:  # ESC
+                break
+            number, mode = select_mode(key, mode)
+
+            # Camera capture #####################################################
+            ret, image = cap.read()
+            if not ret:
+                break
+            image = cv.flip(image, 1)  # Mirror display
+            debug_image = copy.deepcopy(image)
+
+            # Detection implementation #############################################################
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+            image.flags.writeable = False
+            results = hands.process(image)
+            image.flags.writeable = True
+
+            #  ####################################################################
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks, handedness in zip(
+                    results.multi_hand_landmarks, results.multi_handedness
+                ):
+                    # Bounding box calculation
+                    brect = calc_bounding_rect(debug_image, hand_landmarks)
+                    # Landmark calculation
+                    landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+                    # Conversion to relative coordinates / normalized coordinates
+                    pre_processed_landmark_list = pre_process_landmark(landmark_list)
+                    # Write to the dataset file
+                    logging_csv(number, mode, pre_processed_landmark_list)
+
+                    # Hand sign classification
+                    hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+                    # Drawing part
+                    debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                    debug_image = draw_landmarks(debug_image, landmark_list)
+                    debug_image = draw_info_text(
+                        debug_image,
+                        brect,
+                        handedness,
+                        keypoint_classifier_labels[hand_sign_id],
+                    )
+
+            debug_image = draw_info(debug_image, mode, number)
+
+            # Screen reflection #############################################################
+            cv.imshow("Hand Gesture Recognition", debug_image)
+
+        cap.release()
+        cv.destroyAllWindows()
 
 
 def select_mode(key, mode):
